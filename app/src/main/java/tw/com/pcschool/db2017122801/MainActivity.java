a29b1791f2fc0ba558d4194c8476f71febc1fc95
Teacher <shaojung@gmail.com>
2017-12-28 10:22:34 +0800

    Initial commit

---
+++ app/src/main/java/tw/com/pcschool/db2017122801/MainActivity.java
@@ -0,0 +1,20 @@
+package tw.com.pcschool.db2017122801;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void click1(View v)
+    {
+        tv = findViewById(R.id.textView);
+        tv.setText("123");
+    }
+}
---
+++ app/src/main/java/tw/com/pcschool/db2017122801/SecActivity.java
@@ -0,0 +1,13 @@
+package tw.com.pcschool.db2017122801;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sec);
+    }
+}
